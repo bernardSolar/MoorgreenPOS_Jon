7253744520844fb708d990d4c7408dd89f002412
bernard <bernard.mccarty@gmail.com>
2025-04-09 12:27:57 +0100

    Update app.py to support event parameter in URL

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 from db import init_db, get_products, import_products_from_csv
 import os
+import flask
 
 # Initialize the database (creates tables if needed)
 init_db()
@@ -20,7 +21,8 @@ if not products["Home"]:  # Check if no products exist
 products = get_products()
 
 # Create the Dash app
-app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+server = flask.Flask(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], server=server)
 app.title = "POS System"
 
 # Set config to suppress callback exceptions if requested components don't exist
@@ -30,8 +32,23 @@ app.config.suppress_callback_exceptions = True
 from layout import get_layout
 from callbacks import register_callbacks
 
-# Set the app layout using the products data
-app.layout = get_layout(products)
+# We'll use a function to generate the layout, which lets us check URL parameters
+def serve_layout():
+    # Check if event parameter is in URL query string
+    event_pricing_active = False
+    
+    # Get flask's request context
+    if flask.has_request_context():
+        args = flask.request.args
+        event_param = args.get('event')
+        if event_param == '1':
+            event_pricing_active = True
+    
+    # Get the layout with the appropriate event_pricing_active value
+    return get_layout(products, event_pricing_active)
+
+# Set the app layout using a function so we can check URL parameters
+app.layout = serve_layout
 
 # Register all callbacks with the app
 register_callbacks(app, products)
